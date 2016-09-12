a1eeda21938095b82c7d7d7d4c6501e3e38c8e14
Bryan Kahler <bkahler3@gmail.com>
2016-09-12 02:33:34 +0000

    add stock model and some methods

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,2 +1,24 @@
 class Stock < ActiveRecord::Base
+
+  def self.find_by_ticker(ticker)
+    where(ticker: ticker).first
+  end
+
+  def self.new_from_lookup(ticker)
+    lookup_stock = StockQuote::Stock.quote(ticker)
+    return nil unless lookup_stock.name
+
+    new_stock = new(ticker: lookup_stock.symbol, name: lookup_stock.name)
+    new_stock.last_price = new_stock.price
+    new_stock
+  end
+
+  def price
+    closing_price = StockQuote::Stock.quote(ticker).close
+    return "#{closing_price} (Closing)" if closing_price
+
+    opening_price = StockQuote::Stock.quote(ticker).open
+    return "#{opening_price} (Opening)" if opening_price
+  end
+
 end
