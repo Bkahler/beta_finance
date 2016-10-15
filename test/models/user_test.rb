87bc2bbde7365890a7131e5bc1294f85a118f5c3
Bryan Kahler <bkahler3@gmail.com>
2016-10-15 23:03:12 +0000

    adding first and last name to users

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.create(email: 'tester@test.com', password:'123123')
+    @user2 = User.create(email: 'tester2@test.com', password:'123123')
+  end
+
+  test 'full name should return Anonymous when no names assigned' do
+    full = @user.full_name
+    assert_equal("Anonymous", full)
+  end
+
+  test 'full name should return first + last when names are assigned' do
+    full = @user2.full_name
+    assert_equal("Bryan Kahler", full)
+  end
 end
